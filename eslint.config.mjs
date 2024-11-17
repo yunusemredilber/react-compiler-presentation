95b23356890c89850ac3743539686bac3dd6ed59
yunusemredilber <yunusemredilber@yandex.com>
2024-11-17 15:43:32 +0100

    feat: add react compiler linter to eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -19,6 +20,14 @@ export default [
     rules: {
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
     },
   },
 ];
